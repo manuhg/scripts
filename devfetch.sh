3ace7b3a0df2204d7d2b6f57c1b4f058e55fabd9
manuhg <manu@gk1000.me>
2018-01-22 17:50:36 +0530

    minor changes

--- devfetch.sh
+++ devfetch.sh
@@ -5,10 +5,11 @@ c2="\033[0;34m"
 rc="\033[0;31m"
 nc="\033[0m"
 bold="\033[1m"
+
 url="ssh://gk1000@198.199.121.120:17/home/gk1000"
 repos=("agriaid" "agtrade" "fsort" "gk1000.github.io" "learn" "resources" "sumaagrotech")
 
-for ((i=1;i<${#repos};i++)) do
-    echo $c1"git $url/$c2$bold$repos[$i].git$nc$c1 --recursive$nc"
+for ((i=1;i<=${#repos};i++)) do
+    echo $c1"git clone $url/$c2$bold$repos[$i].git$nc$c1 --recursive$nc"
     git clone "$url/$repos[$i].git" --recursive
     done
