d78af3215e480ed37340676ce20fdba2185068b0
manuhg <manu@gk1000.me>
2018-01-25 17:12:56 +0530

    git auth info

--- git/edit_commit_info.sh
+++ git/edit_commit_info.sh
@@ -1,19 +1,37 @@
 #!/bin/sh
-
-git filter-branch --env-filter -f '
-
-OLD_EMAIL=""
-CORRECT_NAME="manuhg"
-CORRECT_EMAIL="manu@gk1000.me"
-
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
-then
-    export GIT_COMMITTER_NAME="$CORRECT_NAME"
-    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+if [ $# -le 2 ]; then
+	echo "edit_commit_info.sh <old email> <correct name> <correct email>"
+else
+	git filter-branch -f --env-filter '
+	OLD_EMAIL="'+$1+'"
+	CORRECT_NAME="'+$2+'"
+	CORRECT_EMAIL="'+$3+'"
+	if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+	then
+    	export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    	export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+	fi
+	if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+	then
+    	export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    	export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+	fi
+	' --tag-name-filter cat -- --branches --tags
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
-then
-    export GIT_AUTHOR_NAME="$CORRECT_NAME"
-    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
-fi
-' --tag-name-filter cat -- --branches --tags
+# #!/bin/sh
+
+# git filter-branch --env-filter '
+# OLD_EMAIL="your-old-email@example.com"
+# CORRECT_NAME="Your Correct Name"
+# CORRECT_EMAIL="your-correct-email@example.com"
+# if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+# then
+#     export GIT_COMMITTER_NAME="$CORRECT_NAME"
+#     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+# fi
+# if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+# then
+#     export GIT_AUTHOR_NAME="$CORRECT_NAME"
+#     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+# fi
+# ' --tag-name-filter cat -- --branches --tags
