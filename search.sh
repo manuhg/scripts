f57541d7486ef2eb08b3e5d85e6f381c387c6f5b
manu-spherical <manuhegdev@gmail.com>
2018-11-18 11:58:33 +0530

    search

--- search.sh
+++ search.sh
@@ -8,5 +8,6 @@ do
 	echo "Search: "
 	read search_text
 	echo "=============================================================="
-	ls -R '/Volumes/FS 2/Images for website/Uploaded/Free_Download'  | grep -iE '\.(jpeg|jpg)'  | grep -i $search_text| sed -e 's/\.jpg//g;s/\.JPG//g;s/\.jpeg//g;'|sort|uniq|sort
+	#ls -R '/Volumes/FS 2/Images for website/Uploaded/Free_Download'  | grep -iE '\.(jpeg|jpg)'  | grep -i $search_text| sed -e 's/\.jpg//g;s/\.JPG//g;s/\.jpeg//g;'|sort|uniq|sort
+	ls -R '/run/media/gk1000/1010/Webroot/images.shramajeevi/download/Free_Download'  | grep -iE '\.(jpeg|jpg)'  | grep -i $search_text| sed -e 's/\.jpg//g;s/\.JPG//g;s/\.jpeg//g;'|sort|uniq|sort
 done
